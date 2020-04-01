079501e5606abc0183e34669a8ae5407b6034dba
Arthur <a.dhuy@orange.fr>
2020-04-01 12:31:45 +0200

    set up controller

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,16 +1,38 @@
 class CatsController < ApplicationController
+  before_action :find_cat_id, only: [ :update ]
+
   def index
   end
 
   def new
+    @cat = Cat.new
   end
 
   def create
+    @cat = Cat.new(strong_params)
+
+    if @cat.save
+      render partial: "cats/partials/ajax_edit"
+    end
+  end
+
+  def edit
+    render partial: "cats/partials/ajax_edit"
+  end
+
+  def update
+    if @cat.update(strong_params)
+      render :edit
+    end
   end
 
   private
 
   def strong_params
     params.require(:cat).permit(:name, :size, :age, :gender, :color, :gift)
+  end
+
+  def find_cat_id
+    @cat = Cat.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :cats
+  post "/cats", to: "cats#create", as: "create_cat"
+  get "/cat/new", to: "cats#new", as: "new_cat"
+  patch "/cats/:id", to: "cats#update", as: "update_cat"
+  get "/cats/:id/edit", to: "cats#edit", as: "edit_cat"
 end
